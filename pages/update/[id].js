b525a6d39b8b2dae1fa0bef4dcd4707da96b15c2
Corey DeJong <dejongcorey@gmail.com>
2020-07-16 11:32:37 -0700

    update is working!

--- pages/update/[id].js
+++ pages/update/[id].js
@@ -20,13 +20,19 @@ class UpdatedPage extends React.Component{
       user_id_medication:props.schedule.user_id_medication,
     }
     this.handleChangeUser = this.handleChangeUser.bind(this);
+    this.handleChangeMedication = this.handleChangeMedication.bind(this);
+    this.handleChangeDosage = this.handleChangeDosage.bind(this);
+    this.handleChangeHours = this.handleChangeHours.bind(this);
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
+    this.handleChangeUser_Id_Med = this.handleChangeUser_Id_Med.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
   }
 
   async scheduleUpdateHandler(schedule) {
-    console.log("inside the updatehandler with schedule", schedule)
-    const response = await axios.put(url, schedule);
+    // console.log("inside the updatehandler with schedule", schedule)
+    const response = await axios.put(url + this.props.schedule.id, schedule);
     const savedSchedule = response.data;
     this.setState(
       {user:''}
@@ -35,13 +41,49 @@ class UpdatedPage extends React.Component{
   }
 
   handleChangeUser(event){
-    console.log("this is event.targe.value for user", event.target.value)
+    // console.log("this is event.targe.value for user", event.target.value)
     const newUser = event.target.value;
     this.setState({
       user: newUser,
     })
   }
+  handleChangeMedication(event){
+    const newMedication = event.target.value;
+    this.setState({
+      medication: newMedication,
+    })
+  }
+  handleChangeDosage(event){
+    const newDosage = event.target.value;
+    this.setState({
+      dosage: newDosage,
+    })
+  }
+  handleChangeHours(event){
+    const newHours = event.target.value;
+    this.setState({
+      hours: newHours,
+    })
+  }
+  handleChangeStart(event){
+    const newStart = event;
+    this.setState({
+      start: newStart,
+    })
+  }
 
+  handleChangeEnd(event){
+    const newEnd = event;
+    this.setState({
+      end: newEnd,
+    })
+  }
+  handleChangeUser_Id_Med(event){
+    const newUser_Id_Med = event.target.value;
+    this.setState({
+      user_id_medication:newUser_Id_Med,
+    })
+  }  
   handleSubmit(event){
     event.preventDefault();
     console.log("this is inside the handlesubmit", this.state);
@@ -50,6 +92,7 @@ class UpdatedPage extends React.Component{
 
   render(){
     return (
+      <main className={style.medschedule}>
       <form onSubmit={this.handleSubmit}>
         <h1>{this.state.medication}</h1>
         <h3>
@@ -59,8 +102,53 @@ class UpdatedPage extends React.Component{
           User:
           <input name="med-user" type="text" value={this.state.user} onChange={this.handleChangeUser}></input>
         </label> 
+        <label>
+          Medication:
+          <input name="medication" type="text" value={this.state.medication} onChange={this.handleChangeMedication}></input>
+        </label>
+        <label>
+          Your User Medication ID as Text:
+          <input name="med-user_id_med" type="text" value={this.state.user_id_medication} onChange={this.handleChangeUser_Id_Med}></input>
+        </label>
+        <label>
+          Dose:
+          <input name="med-dose" type="text" value={this.state.dosage} onChange={this.handleChangeDosage}></input>
+        </label>
+        <label>
+          Frequency in Hours:
+          <input name="med-hours" type="number" value={this.state.hours} onChange={this.handleChangeHours}></input>
+        </label>
+        {/* <label className="form-group">
+          Start Day:
+          <DatePicker
+              selected={ this.state.start }
+              onChange={ this.handleChangeStart }
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              minDate={new Date()}
+              maxDate={addDays(new Date(), 7)}
+          />
+        </label>
+        <label className="form-group">
+          End Date and Time:
+          <DatePicker
+              selected={ this.state.end }
+              onChange={ this.handleChangeEnd }
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={20}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              minDate={new Date()}
+              maxDate={addDays(new Date(), 7)}
+          />
+        </label> */}
         <button>Submit</button>
       </form> 
+      </main>
     )
   }      
 }
